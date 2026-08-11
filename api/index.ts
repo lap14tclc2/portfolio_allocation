87604346e000e9576444d909ed751df9e5536566
honaz140 <honaz140@gmail.com>
2026-08-11 20:50:58 +0700

    Refactor api/index.ts to directly invoke requestListener without http.Server event emission

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,7 @@
-import { createApp } from '../src/app';
+import { requestListener } from '../src/app';
 import storageService from '../src/services/storage.service';
-
-const server = createApp();
 
 export default async function handler(req: any, res: any) {
   await storageService.initialize();
-  return server.emit('request', req, res);
+  return requestListener(req, res);
 }
-
